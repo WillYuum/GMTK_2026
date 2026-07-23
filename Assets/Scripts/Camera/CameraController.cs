25dc8960211aa888bafa07611a5a04455c620497
WillYuum <liamshere98@gmail.com>
2026-07-23 16:00:53 +0300

    Setup quick camera + pixel perfect

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Camera _mainCamera;
+
+    void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        StartGame();
+    }
+
+ 
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void StartGame()
     {
         
     }
