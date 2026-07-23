1df56e0fc01126d133f72c7ed7c0979e91496bf0
WillYuum <liamshere98@gmail.com>
2026-07-23 21:00:43 +0300

    Add open close panel logic + visual to minigame position

--- Assets/Scripts/MiniGames/MiniGamePosition.cs
+++ Assets/Scripts/MiniGames/MiniGamePosition.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class MiniGamePosition : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer _frontPanel;
+    private Vector3 _initialLocalPosition;
 
+    public bool IsRemoved { get; private set; } = false;
+
+    void Awake()
+    {
+        _initialLocalPosition = _frontPanel.transform.localPosition;
+    }
 
     void Update()
     {
@@ -15,20 +24,52 @@ public class MiniGamePosition : MonoBehaviour
         if (hits.collider != null && hits.collider.TryGetComponent<MiniGamePosition>(out MiniGamePosition miniGamePosition))
         {
             bool isClicked = Mouse.current.leftButton.wasPressedThisFrame;
-            if (isClicked)
+            if (isClicked && miniGamePosition.IsRemoved == false)
             {
-                OnMouseClick();
+                miniGamePosition.OnMouseClick();
             }
         }
 
     }
 
 
-
-
     private void OnMouseClick()
     {
-        //Run Game
+        IsRemoved = true;
+
+        Vector3 start = _frontPanel.transform.localPosition;
+        float xOffset = Random.Range(-0.25f, 0.25f);
+
+        Vector3[] path =
+        {
+            start,
+            // Up
+            start + new Vector3(xOffset * 0.4f, 0.55f, 0f), 
+            // Down + diagonal
+            start + new Vector3(xOffset, -1.2f, 0f)
+        };
+
+        _frontPanel.transform
+            .DOLocalPath(path, 0.8f, PathType.CatmullRom)
+            .SetEase(Ease.InQuart);
+
+        _frontPanel
+            .DOFade(0f, 0.45f)
+            .SetDelay(0.6f);
+    }
+
+
+    public void PlaceBackPanel()
+    {
+        IsRemoved = false;
+
+        _frontPanel.DOKill();
+        _frontPanel.transform.DOKill();
+
+        _frontPanel.DOFade(1f, 0.2f);
+        _frontPanel.transform
+            .DOLocalMove(_initialLocalPosition, 0.25f)
+            .SetEase(Ease.OutBack);
     }
 
 }
