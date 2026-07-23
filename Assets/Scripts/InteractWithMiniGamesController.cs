f97200d99dfe45dcf677dcbe733aede23dc760da
WillYuum <liamshere98@gmail.com>
2026-07-23 22:56:06 +0300

    Change responsibility for interacting with a panel

--- Assets/Scripts/InteractWithMiniGamesController.cs
+++ Assets/Scripts/InteractWithMiniGamesController.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InteractWithMiniGamesController : MonoBehaviour
 {
 
+
+    void Update()
+    {
+        Vector2 mousePosInWorld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        //if mouse hover
+        var hits = Physics2D.Raycast(mousePosInWorld, Vector2.zero);
+
+        if (hits.collider != null && hits.collider.TryGetComponent(out MiniGamePanel miniGamePosition))
+        {
+            bool isClicked = Mouse.current.leftButton.wasPressedThisFrame;
+            if (isClicked && miniGamePosition.IsRemoved == false)
+            {
+                miniGamePosition.RemovePanel();
+            }
+        }
+
+    }
 }
--- Assets/Scripts/MiniGames/MiniGamePanel.cs
+++ Assets/Scripts/MiniGames/MiniGamePanel.cs
@@ -14,26 +14,7 @@ public class MiniGamePanel : MonoBehaviour
         _initialLocalPosition = _frontPanel.transform.localPosition;
     }
 
-    void Update()
-    {
-        Vector2 mousePosInWorld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-
-        //if mouse hover
-        var hits = Physics2D.Raycast(mousePosInWorld, Vector2.zero);
-
-        if (hits.collider != null && hits.collider.TryGetComponent(out MiniGamePanel miniGamePosition))
-        {
-            bool isClicked = Mouse.current.leftButton.wasPressedThisFrame;
-            if (isClicked && miniGamePosition.IsRemoved == false)
-            {
-                miniGamePosition.OnMouseClick();
-            }
-        }
-
-    }
-
-
-    private void OnMouseClick()
+    public void RemovePanel()
     {
         IsRemoved = true;
 
