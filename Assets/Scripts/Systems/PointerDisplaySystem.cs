6b6103bafe129f9785bd559e4e1ca7ebbee95a52
WillYuum <liamshere98@gmail.com>
2026-07-25 14:56:36 +0300

    Fix cursor position

--- Assets/Scripts/Systems/PointerDisplaySystem.cs
+++ Assets/Scripts/Systems/PointerDisplaySystem.cs
@@ -20,32 +20,35 @@ public interface IHoverInteractable
 
 public class PointerDisplaySystem : MonoBehaviour
 {
-    [Header("Cursor")]
-    [SerializeField] private SpriteRenderer _cursorRenderer;
+    [System.Serializable]
+    public struct PointerData
+    {
+        public PointerDisplayType Type;
+        public Sprite Sprite;
+        [Tooltip("Additional manual tweak on top of automatic baseline alignment")]
+        public Vector2 FineTuneOffset;
+    }
 
-    [Header("Cursor Sprites")]
-    [SerializeField] private Sprite _defaultPointer;
-    [SerializeField] private Sprite _toolPointer;
-    [SerializeField] private Sprite _toolHand;
-    [SerializeField] private Sprite _toolBroom;
-    [SerializeField] private Sprite _toolScrewdriver;
+    [Header("Cursor Settings")]
+    [SerializeField] private SpriteRenderer _cursorRenderer;
+    [SerializeField] private PointerData[] _pointers;
 
     private Camera _camera;
     private IHoverInteractable _currentHover;
+    private PointerDisplayType _currentType = PointerDisplayType.Default;
 
     private void Awake()
     {
         _camera = Camera.main;
-
-        Cursor.visible = false;
+        // Cursor.visible = false;
     }
 
-    void Start()
+    private void Start()
     {
         SetDefaultPointer();
     }
 
-    void LateUpdate()
+    private void LateUpdate()
     {
         UpdateCursorPosition();
         UpdateHover();
@@ -54,10 +57,60 @@ public class PointerDisplaySystem : MonoBehaviour
     private void UpdateCursorPosition()
     {
         Vector3 mousePosition = Mouse.current.position.ReadValue();
-
         mousePosition.z = -_camera.transform.position.z;
 
-        _cursorRenderer.transform.position = _camera.ScreenToWorldPoint(mousePosition);
+        Vector3 worldPos = _camera.ScreenToWorldPoint(mousePosition);
+
+        Vector2 autoOffset = GetAutomaticOffset(_currentType);
+
+        Vector2 manualOffset = GetCurrentManualOffset(_currentType);
+
+
+        worldPos.x += autoOffset.x + manualOffset.x;
+        worldPos.y += autoOffset.y + manualOffset.y;
+
+        _cursorRenderer.transform.position = worldPos;
+    }
+
+    private Vector2 GetAutomaticOffset(PointerDisplayType type)
+    {
+        if (_cursorRenderer.sprite == null) return Vector2.zero;
+
+        Vector2 spriteSize = _cursorRenderer.sprite.bounds.size;
+        Vector2 pivot = _cursorRenderer.sprite.pivot / _cursorRenderer.sprite.rect.size;
+
+        float yOffset = -(spriteSize.y * (1f - pivot.y));
+        float xOffset;
+
+        switch (type)
+        {
+            case PointerDisplayType.ToolScrewdriver:
+            case PointerDisplayType.ToolBroom:
+                xOffset = spriteSize.x * (0.5f - pivot.x);
+                break;
+
+            case PointerDisplayType.Default:
+            case PointerDisplayType.ToolPointer:
+            case PointerDisplayType.ToolHand:
+            default:
+                xOffset = -(spriteSize.x * pivot.x);
+                break;
+        }
+
+        return new Vector2(xOffset, yOffset);
+    }
+
+    private Vector2 GetCurrentManualOffset(PointerDisplayType type)
+    {
+        for (int i = 0; i < _pointers.Length; i++)
+        {
+            if (_pointers[i].Type == type)
+            {
+                return _pointers[i].FineTuneOffset;
+            }
+        }
+
+        return Vector2.zero;
     }
 
     private void UpdateHover()
@@ -65,7 +118,6 @@ public class PointerDisplaySystem : MonoBehaviour
         Vector2 mousePosition = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
         IHoverInteractable newHover = null;
-
         RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
         if (hit.collider != null)
@@ -74,10 +126,11 @@ public class PointerDisplaySystem : MonoBehaviour
         }
 
         if (_currentHover == newHover)
+        {
             return;
+        }
 
         _currentHover?.OnHoverExit();
-
         _currentHover = newHover;
 
         if (_currentHover != null)
@@ -93,24 +146,22 @@ public class PointerDisplaySystem : MonoBehaviour
 
     public void SetPointer(PointerDisplayType type)
     {
-        _cursorRenderer.sprite = GetSprite(type);
+        _currentType = type;
+
+        for (int i = 0; i < _pointers.Length; i++)
+        {
+            if (_pointers[i].Type == type)
+            {
+                _cursorRenderer.sprite = _pointers[i].Sprite;
+                return;
+            }
+        }
+
+        SetDefaultPointer();
     }
 
     public void SetDefaultPointer()
     {
         SetPointer(PointerDisplayType.Default);
-    }
-
-    private Sprite GetSprite(PointerDisplayType type)
-    {
-        return type switch
-        {
-            PointerDisplayType.Default => _defaultPointer,
-            PointerDisplayType.ToolPointer => _toolPointer,
-            PointerDisplayType.ToolHand => _toolHand,
-            PointerDisplayType.ToolBroom => _toolBroom,
-            PointerDisplayType.ToolScrewdriver => _toolScrewdriver,
-            _ => _defaultPointer
-        };
     }
 }
