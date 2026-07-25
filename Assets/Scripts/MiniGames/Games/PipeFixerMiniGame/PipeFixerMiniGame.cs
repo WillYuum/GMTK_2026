a47a33fd3c9830934934f95cc89bb59af92224eb
WillYuum <liamshere98@gmail.com>
2026-07-25 22:17:36 +0300

    Create files for new pipe game

---
+++ Assets/Scripts/MiniGames/Games/PipeFixerMiniGame/PipeFixerMiniGame.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PipeFixerMiniGame : MiniGame
+{
+    public override void OnEnd()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnInitialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
