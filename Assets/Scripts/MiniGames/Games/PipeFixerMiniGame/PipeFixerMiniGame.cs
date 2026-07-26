2fae73f24baf40a392d7f2efea9fa87310947fb5
WillYuum <liamshere98@gmail.com>
2026-07-26 18:32:44 +0300

    Play rotate pip sfx

--- Assets/Scripts/MiniGames/Games/PipeFixerMiniGame/PipeFixerMiniGame.cs
+++ Assets/Scripts/MiniGames/Games/PipeFixerMiniGame/PipeFixerMiniGame.cs
@@ -83,6 +83,7 @@ public class PipeFixerMiniGame : MiniGame
             return;
         }
 
+        AudioManager.Instance.PlaySFX("Rotate_Pipe");
         pipe.HandleRotateClockWise();
 
         if (CheckIfAllPipesAligned())
